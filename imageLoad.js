abf33397610dca39095a208f187b92ea115efe5c
Saitej Erupaka <serupaka@gmail.com>
2016-04-28 02:50:08 -0500

    add tags

---
+++ imageLoad.js
@@ -0,0 +1,10 @@
+
+// var ctx = can.getContext('2d');
+
+// var img = new Image();
+// img.onload = function(){
+//     can.width = 300;
+//     can.height = 400;
+//     ctx.drawImage(img, 0, 0, img.width, img.height, 300, 400);
+// }
+// img.src = '1.jpg';
